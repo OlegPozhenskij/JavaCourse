a7574290bb45211d41a6cac1f9bae29bcd84c9a8
Oleg <57754088+OlegPozhenskij@users.noreply.github.com>
2024-03-08 16:10:27 +0400

    Добавил пропущенный js файл

---
+++ src/main/java/teamscore/pozhenskij2/webapp/task3/_js/oil.js
@@ -0,0 +1,75 @@
+const oilPrices = [55.2, 54.8, 54.5, 55.1, 55.6, 56.2, 56.5, 56.8, 57.1, 57.5];
+
+const tableBody = document.getElementById("tableBody");
+const maxPriceSpan = document.getElementById("maxPrice");
+const minPriceSpan = document.getElementById("minPrice");
+const averageChangeSpan = document.getElementById("averageChange");
+const averageIndexSpan = document.getElementById("averageIndex");
+const positiveChangesSpan = document.getElementById("positiveChanges");
+const negativeChangesSpan = document.getElementById("negativeChanges");
+
+let maxPrice = Math.max(...oilPrices);
+let minPrice = Math.min(...oilPrices);
+maxPriceSpan.textContent = maxPrice;
+minPriceSpan.textContent = minPrice;
+
+let totalChange = 0;
+let totalIndex = 0;
+let positiveChanges = 0;
+let negativeChanges = 0;
+
+tableBody.innerHTML = `
+<tr>
+  <td>1</td>
+  <td> - </td>
+  <td>${oilPrices[0]}</td>
+  <td> - </td>
+  <td> - </td>
+  <td> - </td>
+</tr>
+`;
+
+for (let i = 1; i < oilPrices.length; i++) {
+  let change = oilPrices[i] - oilPrices[i - 1];
+  let index = oilPrices[i] / oilPrices[i - 1];
+  let changeRate = (index - 1) * 100;
+
+  let trend;
+  if (change > 0) {
+    trend = '<span class="up-arrow">&#8682;</span>';
+    positiveChanges++;
+  } else if (change < 0) {
+    trend = '<span class="down-arrow">&#8681;</span>';
+    negativeChanges++;
+  } else {
+    trend = "-";
+  }
+  totalChange += change;
+  totalIndex += index;
+
+  let row = `
+    <tr>
+      <td>${i + 1}</td>
+      <td>${trend}</td>
+      <td>${oilPrices[i]}</td>
+      <td>${change.toFixed(2)}</td>
+      <td>${index.toFixed(2)}</td>
+      <td>${changeRate.toFixed(2)}%</td>
+    </tr>
+  `;
+
+  tableBody.innerHTML += row;
+}
+
+let tableRows = document.getElementById("tableBody").getElementsByTagName('tr');
+
+for(let i = 0; i < tableRows.length; i++) {
+  if(i % 2 == 0) tableRows[i].classList.add('gray')
+}
+
+averageChangeSpan.textContent = (totalChange / (oilPrices.length - 1)).toFixed(
+  2
+);
+averageIndexSpan.textContent = (totalIndex / (oilPrices.length - 1)).toFixed(2);
+positiveChangesSpan.textContent = positiveChanges;
+negativeChangesSpan.textContent = negativeChanges;
